623fcf89475635d6966396c8caeae62e12bc7f31
minju.H <105367622+minjuuH@users.noreply.github.com>
2024-07-18 13:37:07 +0900

    모달창 버튼 텍스트 수정
    - 클릭한 버튼에 따라 버튼 텍스트 변경

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     const signinButton = document.getElementById('signinBtn');
     const signupButton = document.getElementById('signupBtn');
+    const btnText = document.getElementById('btnTxt');
     const modal = document.getElementById('loginModal');
     const title = document.getElementById('modalTitle');
     //const closeBtn = document.querySelector('.close');
@@ -10,11 +11,13 @@ document.addEventListener('DOMContentLoaded', ()=>{
     signinButton.addEventListener('click', ()=>{
         modal.style.display = 'flex';
         title.textContent = "로그인";
+        btnText.textContent = '로그인';
     });
 
     signupButton.addEventListener('click', ()=>{
         modal.style.display = 'flex';
         title.textContent = "회원가입";
+        btnText.textContent = '회원가입';
     });
     
     // closeBtn.addEventListener('click', ()=>{
